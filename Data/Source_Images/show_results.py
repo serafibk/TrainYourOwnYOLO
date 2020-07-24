a14929e527620fb1ad5429232e279a5a37d8b72e
Serafina <serafibk@umich.edu>
2020-07-24 14:17:58 -0400

    post processing on show_results to improve precision

--- Data/Source_Images/show_results.py
+++ Data/Source_Images/show_results.py
@@ -17,8 +17,8 @@ xml_folder = "xml_bound_train"
 def calc_IoU(xml,proposed):
     intersection = 0
     xmlMinX = xml[0]
-    xmlMinY = xml[2]
-    xmlMaxX = xml[1]
+    xmlMinY = xml[1]
+    xmlMaxX = xml[2]
     xmlMaxY = xml[3]
     propMinX = proposed[0]
     propMinY = proposed[1]
@@ -41,13 +41,20 @@ images.sort()
 num_tables_prop = 0
 correct_tables = 0
 
+delta = 5
+
+tables_on_page = {}
+
 #to read csv
 with open(os.path.join(root_path,"Test_Image_Detection_Results/1000_result/Detection_Results.csv"),'r') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         num = row[0][16:-4]
         if num: #exclude header row
-            proposed=[int(row[2]),int(row[3]),int(row[4]),int(row[5])]#minX minY maxX maxY
+            checking_table=True
+            page_width = int(row[8])
+            page_height = int(row[9])
+            proposed=[max(int(row[2])-delta,0),max(int(row[3])-delta,0),min(int(row[4])+delta,page_width),min(int(row[5])+delta,page_height)]#minX minY maxX maxY
             top_left = (proposed[0],proposed[1])
             top_right = (proposed[2],proposed[1])
             bot_left = (proposed[0],proposed[3])
@@ -55,68 +62,117 @@ with open(os.path.join(root_path,"Test_Image_Detection_Results/1000_result/Detec
 
             confidence = float(row[7])
 
-            ##get corresponding xml to calculate IoU score
-            xml_name = "train_bound_xml_"+num+".xml"
+            if num in tables_on_page:
+                max_iou = 0
+                prop_overlap=[]
+                found_ind = 0
+                for i,prop in enumerate(tables_on_page[num]):
 
-            maxIOU = 0 #find max of all tables in xml compared to proposed
+                    iou = calc_IoU(prop[0],proposed)
+                    if iou>max_iou:
+                        max_iou = iou
+                        prop_overlap = prop[0]
+                        found_ind = i
+                if max_iou < 0.1: #doesn't overlap with already proposed tables
+                    tables_on_page[num].append([proposed,confidence])
 
-            #read in bounds
-            with open(os.path.join(root_xml,xml_folder,xml_name),'r') as xml:
-                tree = ET.parse(xml)
+                elif prop[1] < confidence: #confidence is higher
+                    tables_on_page[num].append([proposed,confidence])
+                    del tables_on_page[num][found_ind]
 
-                root = tree.getroot()
-                for table in root:
-                    points = table[0].attrib["points"]
-
-                    min_vals = [10000, 10000]
-                    max_vals = [0,0]
-
-                    on_X = True
-                    temp = ""
-                    for char in points:
-                        if(char == "," or char == " "):
-                            if(int(temp) < min_vals[not on_X]):
-                                min_vals[not on_X] = int(temp)
-                            if(int(temp) > max_vals[not on_X]):
-                                max_vals[not on_X] = int(temp)
-                            on_X = not on_X
-                            temp = ""
-                        else:
-                            temp += char
-                    xml_locs = [min_vals[0],max_vals[0],min_vals[1],max_vals[1]]#minX maxX minY maxY
-                    iou = calc_IoU(xml_locs,proposed)
-                    if iou > maxIOU:
-                        maxIOU = iou
-            im_name = "train_bound_img_"+num+"_catface.jpg"
-            image = cv2.imread(os.path.join(root_path,image_folder,im_name),0)
-            cv2.line(image, top_left, bot_left, (0,255,0), 3)
-            cv2.line(image, top_left, top_right, (0,255,0), 3)
-            cv2.line(image, bot_left, bot_right, (0,255,0), 3)
-            cv2.line(image, top_right, bot_right, (0,255,0), 3)
-            image = cv2.resize(image,(600,800))
-            cv2.imshow("image",image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
-            #if maxIOU > threshold, correctly identifies table
-            if maxIOU > 0.6:
-                im_name = "train_bound_img_"+num+"_catface.jpg"
-                image = cv2.imread(os.path.join(root_path,image_folder,im_name),0)
-                '''
-                cv2.line(image, top_left, bot_left, (0,255,0), 3)
-                cv2.line(image, top_left, top_right, (0,255,0), 3)
-                cv2.line(image, bot_left, bot_right, (0,255,0), 3)
-                cv2.line(image, top_right, bot_right, (0,255,0), 3)
-                image = cv2.resize(image,(600,800))
-                '''
-                if maxIOU <= 0.7:
                     '''
+                    top_left_over = (prop_overlap[0],prop_overlap[1])
+                    bot_right_over = (prop_overlap[2],prop_overlap[3])
+                    print("new table is more confident")
+                    im_name = "train_bound_img_"+num+"_catface.jpg"
+                    image = cv2.imread(os.path.join(root_path,image_folder,im_name))
+                    cv2.rectangle(image,top_left,bot_right,(0,255,0), 3)
+                    cv2.rectangle(image,top_left_over,bot_right_over,(0,0,255), 3)
+                    image = cv2.resize(image,(600,800))
                     cv2.imshow("image",image)
                     cv2.waitKey(0)
                     cv2.destroyAllWindows()
                     '''
-                correct_tables+=1
-            num_tables_prop+=1
+                else:
+                    checking_table=False #dont add to list of tables proposed
+                    top_left_over = (prop_overlap[0],prop_overlap[1])
+                    bot_right_over = (prop_overlap[2],prop_overlap[3])
+                    print("overlap and less confident")
+                    im_name = "train_bound_img_"+num+"_catface.jpg"
+                    image = cv2.imread(os.path.join(root_path,image_folder,im_name))
+                    cv2.rectangle(image,top_left,bot_right,(0,0,255), 3)
+                    cv2.rectangle(image,top_left_over,bot_right_over,(0,255,0), 3)
+                    image = cv2.resize(image,(600,800))
+                    cv2.imshow("image",image)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+            else:
+                tables_on_page[num] = [[proposed,confidence]]
+
+
+for num in tables_on_page:
+    for prop in tables_on_page[num]:
+
+        ##get corresponding xml to calculate IoU score
+        xml_name = "train_bound_xml_"+num+".xml"
+
+        maxIOU = 0 #find max of all tables in xml compared to proposed
+
+        #read in bounds
+        with open(os.path.join(root_xml,xml_folder,xml_name),'r') as xml:
+            tree = ET.parse(xml)
+
+            root = tree.getroot()
+            for table in root:
+                points = table[0].attrib["points"]
+
+                min_vals = [10000, 10000]
+                max_vals = [0,0]
+
+                on_X = True
+                temp = ""
+                for char in points:
+                    if(char == "," or char == " "):
+                        if(int(temp) < min_vals[not on_X]):
+                            min_vals[not on_X] = int(temp)
+                        if(int(temp) > max_vals[not on_X]):
+                            max_vals[not on_X] = int(temp)
+                        on_X = not on_X
+                        temp = ""
+                    else:
+                        temp += char
+                xml_locs = [min_vals[0],min_vals[1],max_vals[0],max_vals[1]]#minX minY maxX, maxY
+                iou = calc_IoU(xml_locs,prop[0])
+                if iou > maxIOU:
+                    maxIOU = iou
+        '''
+        top_left = (prop[0][0],prop[0][1])
+        bot_right = (prop[0][2],prop[0][3])
+        im_name = "train_bound_img_"+num+"_catface.jpg"
+        image = cv2.imread(os.path.join(root_path,image_folder,im_name))
+        cv2.rectangle(image, top_left, bot_right, (255,0,0), 3)
+        image = cv2.resize(image,(600,800))
+        cv2.imshow("image",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        '''
+
+
+        #if maxIOU > threshold, correctly identifies table
+        if maxIOU > 0.7:
+            '''
+            im_name = "train_bound_img_"+num+"_catface.jpg"
+            image = cv2.imread(os.path.join(root_path,image_folder,im_name))
+            top_left = (prop[0][0],prop[0][1])
+            bot_right = (prop[0][2],prop[0][3])
+            cv2.rectangle(image, top_left, bot_right, (255,0,0), 3)
+            image = cv2.resize(image,(600,800))
+            cv2.imshow("image",image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            '''
+            correct_tables+=1
+        num_tables_prop+=1
 
 #count ground truth tables
 num_tables_xml=0
